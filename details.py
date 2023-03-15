a91770b7c0a433df5db9b0f18c21765a03970741
eps1loN <90094326+0siradmin0@users.noreply.github.com>
2023-03-15 21:59:01 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,3 +1,3 @@
 api_id = "22992731"
 api_hash = "44e54ee37466dcb82aab67f5a6123416"
-bot_token = "6177801742:AAGaa9hq6unoAh_mGRXgsDKIjM6dfNhnXkQ"
+bot_token = "6222473868:AAH2OCA-re-cFyfXCR9xwDOkkj7e1LHT7g8"
